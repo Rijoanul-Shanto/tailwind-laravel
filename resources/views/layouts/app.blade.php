52ae4ef917966aabb0a2b1847e8f4c40a2532b0d
Rijoanul-Shanto <rijoanul.shanto@gmail.com>
2020-12-31 19:23:34 +0600

    initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <div class="m-6 space-x-3 space-y-3">
+        <button
+            class="inline-block px-6 py-2 text-xs font-medium leading-6 text-center text-white uppercase transition bg-blue-700 rounded shadow ripple hover:shadow-lg hover:bg-blue-800 focus:outline-none"
+        >
+            Primary
+        </button>
+    </div>
+</body>
+</html>
