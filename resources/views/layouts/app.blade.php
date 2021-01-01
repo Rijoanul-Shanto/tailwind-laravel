6253f2b41ef11a4cae36fcc38fa39a5aabb4ad29
Rijoanul-Shanto <rijoanul.shanto@gmail.com>
2021-01-01 10:24:43 +0600

    staged the main branch

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,12 +7,6 @@
     <title>Document</title>
 </head>
 <body>
-    <div class="m-6 space-x-3 space-y-3">
-        <button
-            class="inline-block px-6 py-2 text-xs font-medium leading-6 text-center text-white uppercase transition bg-blue-700 rounded shadow ripple hover:shadow-lg hover:bg-blue-800 focus:outline-none"
-        >
-            Primary
-        </button>
-    </div>
+    <h4>Hello World</h4>
 </body>
 </html>
